cecdd3c32727efcbbecd39522c5bad13f09d1998
Justin Grote <@justinwgrote>
2023-01-12 19:00:56 -0800

    Fix tests to focus on the temp install path

--- ModuleFast.tests.ps1
+++ ModuleFast.tests.ps1
@@ -350,6 +350,17 @@ Describe 'Get-ModuleFastPlan' -Tag 'E2E' {
 Describe 'Install-ModuleFast' -Tag 'E2E' {
 	BeforeAll {
 		$SCRIPT:__existingPSModulePath = $env:PSModulePath
+		filter Limit-ModulePath {
+			param(
+				[string]$path,
+
+				[Parameter(ValueFromPipeline)]
+				[Management.Automation.PSModuleInfo]$InputObject
+			)
+			if ($PSItem.Path.StartsWith($path)) {
+				return $PSItem
+			}
+		}
 	}
 	BeforeEach {
 		#Remove all PSModulePath to not affect existing environment
@@ -383,7 +394,9 @@ Describe 'Install-ModuleFast' -Tag 'E2E' {
 			$moduleFolderVersion = $_ | Split-Path | Split-Path -Leaf
 			Import-PowerShellDataFile -Path $_.FullName | ForEach-Object ModuleVersion | Should -Be $moduleFolderVersion
 		}
-		Get-Module VMWare* -ListAvailable | Should -HaveCount 2
+		Get-Module VMWare* -ListAvailable
+		| Limit-ModulePath $installTempPath
+		| Should -HaveCount 2
 	}
 	It 'lots of dependencies (Az)' {
 		Install-ModuleFast @imfParams 'Az'
@@ -391,19 +404,28 @@ Describe 'Install-ModuleFast' -Tag 'E2E' {
 	}
 	It 'specific requiredVersion' {
 		Install-ModuleFast @imfParams @{ ModuleName = 'Az.Accounts'; RequiredVersion = '2.7.4' }
-		Get-Module Az.Accounts -ListAvailable | Select-Object -ExpandProperty Version | Should -Be '2.7.4'
+		Get-Module Az.Accounts -ListAvailable
+		| Limit-ModulePath $installTempPath
+		| Select-Object -ExpandProperty Version
+		| Should -Be '2.7.4'
 	}
 	It 'specific requiredVersion when newer version is present' {
 		Install-ModuleFast @imfParams 'Az.Accounts'
 		Install-ModuleFast @imfParams @{ ModuleName = 'Az.Accounts'; RequiredVersion = '2.7.4' }
-		$installedVersions = Get-Module Az.Accounts -ListAvailable | Select-Object -ExpandProperty Version
+		$installedVersions = Get-Module Az.Accounts -ListAvailable
+		| Limit-ModulePath $installTempPath
+		| Select-Object -ExpandProperty Version
+
 		$installedVersions | Should -HaveCount 2
 		$installedVersions | Should -Contain '2.7.4'
 	}
 	It 'Installs when Maximumversion is lower than currently installed' {
 		Install-ModuleFast @imfParams 'Az.Accounts'
 		Install-ModuleFast @imfParams @{ ModuleName = 'Az.Accounts'; MaximumVersion = '2.7.3' }
-		Get-Module Az.Accounts -ListAvailable | Select-Object -ExpandProperty Version | Should -Contain '2.7.3'
+		Get-Module Az.Accounts -ListAvailable
+		| Limit-ModulePath $installTempPath
+		| Select-Object -ExpandProperty Version
+		| Should -Contain '2.7.3'
 	}
 	It 'Only installs once when Update is specified and latest has not changed' {
 		Install-ModuleFast @imfParams 'Az.Accounts' -Update
@@ -413,15 +435,17 @@ Describe 'Install-ModuleFast' -Tag 'E2E' {
 
 	It 'Updates only dependent module that requires update' {
 		Install-ModuleFast @imfParams @{ ModuleName = 'Az.Accounts'; RequiredVersion = '2.10.2' }
-		Install-ModuleFast @imfParams	@{ ModuleName = 'Az.Compute'; RequiredVersion = '5.0.0'}
+		Install-ModuleFast @imfParams	@{ ModuleName = 'Az.Compute'; RequiredVersion = '5.0.0' }
 		Get-Module Az.Accounts -ListAvailable
+		| Limit-ModulePath $installTempPath
 		| Select-Object -ExpandProperty Version
 		| Sort-Object Version -Descending
 		| Select-Object -First 1
 		| Should -Be '2.10.2'
 
-		Install-ModuleFast @imfParams 'Az.Compute','Az.Accounts' #Should not update
+		Install-ModuleFast @imfParams 'Az.Compute', 'Az.Accounts' #Should not update
 		Get-Module Az.Accounts -ListAvailable
+		| Limit-ModulePath $installTempPath
 		| Select-Object -ExpandProperty Version
 		| Sort-Object Version -Descending
 		| Select-Object -First 1
@@ -429,12 +453,14 @@ Describe 'Install-ModuleFast' -Tag 'E2E' {
 
 		Install-ModuleFast @imfParams 'Az.Compute' -Update #Should disregard local install and update latest Az.Accounts
 		Get-Module Az.Accounts -ListAvailable
+		| Limit-ModulePath $installTempPath
 		| Select-Object -ExpandProperty Version
 		| Sort-Object Version -Descending
 		| Select-Object -First 1
 		| Should -BeGreaterThan ([version]'2.10.2')
 
 		Get-Module Az.Compute -ListAvailable
+		| Limit-ModulePath $installTempPath
 		| Select-Object -ExpandProperty Version
 		| Sort-Object Version -Descending
 		| Select-Object -First 1
