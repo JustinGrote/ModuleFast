4c625f64ece5938c38ff67105b65ae7392ea603f
Justin Grote <JustinGrote@users.noreply.github.com>
2023-03-22 23:53:38 -0700

    Fix Destination line formatting

--- ModuleFast.psm1
+++ ModuleFast.psm1
@@ -1148,7 +1148,10 @@ function Add-DestinationToPSModulePath {
 		New-Item -ItemType File -Path $myProfile -Force | Out-Null
 	}
 
-	[string]$profileLine = {if ('$Destination' -notin ($env:PSModulePath.split([IO.Path]::PathSeparator))) {$env:PSModulePath = '$Destination' + $([IO.Path]::PathSeparator + $env:PSModulePath)} <#Added by ModuleFast. DO NOT EDIT THIS LINE. If you do not want this, add -NoProfileUpdate to Install-ModuleFast or add the default destination to your powershell.config.json or to your PSModulePath another way.#> }
+	[string]$profileLine = {if ('##DESTINATION##' -notin ($env:PSModulePath.split([IO.Path]::PathSeparator))) {$env:PSModulePath = '##DESTINATION##' + $([IO.Path]::PathSeparator + $env:PSModulePath)} <#Added by ModuleFast. DO NOT EDIT THIS LINE. If you do not want this, add -NoProfileUpdate to Install-ModuleFast or add the default destination to your powershell.config.json or to your PSModulePath another way.#> }
+
+  #We can't use string formatting because of the braces already present
+  $profileLine = $profileLine -replace '##DESTINATION##', $Destination
 
 	if ((Get-Content -Raw $myProfile) -notmatch [Regex]::Escape($ProfileLine)) {
 		if (-not $PSCmdlet.ShouldProcess($myProfile, "Allow ModuleFast to work by adding $Destination to your PSModulePath on startup by appending to your CurrentUserAllHosts profile. If you do not want this, add -NoProfileUpdate to Install-ModuleFast or add the specified destination to your powershell.config.json or to your PSModulePath another way.")) { return }
