dfea1175b4e5e86d803cac728abd98dfa9ab73e3
Justin Grote <JustinGrote@users.noreply.github.com>
2026-07-10 03:18:21 +0000

    Try with confirm

--- build.ps1
+++ build.ps1
@@ -2,7 +2,7 @@ $ErrorActionPreference = 'Stop'
 
 #Use the simple v1 for now for bootstrapping
 $v0ModuleFastUri = 'https://github.com/JustinGrote/ModuleFast/releases/download/v0.6.1/ModuleFast.ps1'
-& ([ScriptBlock]::Create((iwr $v0ModuleFastUri))) -path ./ModuleFastBuild.requires.psd1
+& ([ScriptBlock]::Create((iwr $v0ModuleFastUri))) -path ./ModuleFastBuild.requires.psd1 -Confirm:$false
 
 Push-Location $PSScriptRoot
 try {
