1d1e35287bff7530b4f414453fa9dcb8d8c57f73
Justin Grote <JustinGrote@users.noreply.github.com>
2026-07-10 03:26:51 +0000

    Bump rev and try again

--- build.ps1
+++ build.ps1
@@ -1,8 +1,12 @@
 $ErrorActionPreference = 'Stop'
 
-#Use the simple v1 for now for bootstrapping
-$v0ModuleFastUri = 'https://github.com/JustinGrote/ModuleFast/releases/download/v0.6.1/ModuleFast.ps1'
-& ([ScriptBlock]::Create((iwr $v0ModuleFastUri))) -path ./ModuleFastBuild.requires.psd1 -Confirm:$false
+#Run in a separate job to avoid keeping the legacy nuget versioning assembly in memory.
+Start-Job {
+  #Use the simple v1 for now for bootstrapping
+  $v0ModuleFastUri = 'https://github.com/JustinGrote/ModuleFast/releases/download/v0.6.1/ModuleFast.ps1'
+  iwr $v0ModuleFastUri | iex
+  Install-ModuleFast
+} | Receive-Job -Wait -AutoRemoveJob
 
 Push-Location $PSScriptRoot
 try {
