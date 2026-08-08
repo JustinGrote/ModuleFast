4787e087bda4c57a9442ab20f4d9e4c50cc60a5d
Justin Grote <JustinGrote@users.noreply.github.com>
2026-08-08 05:15:34 +0000

    📄 Add base site configuration

--- Source/Docs/astro.config.mjs
+++ Source/Docs/astro.config.mjs
@@ -4,6 +4,8 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
+	site: 'https://justingrote.github.io',
+	base: '/ModuleFast/',
 	integrations: [
 		starlight({
 			title: 'My Docs',
